d7ebd4202fa8e2691d7fa5bb15550f4b2d9de308
Thales Pereira <thalespereiradejesus@gmail.com>
2022-04-13 19:14:18 -0300

    :wrench: Add default adapter on application and tests

--- config/test.exs
+++ config/test.exs
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Use mock adapter for Address client
+config :tesla, Project.Infrastructure.AddressClient, adapter: Tesla.Mock
