534826c7edd6e78a9a13f9fa67367789698bac60
Thales Pereira <thalespereiradejesus@gmail.com>
2022-04-13 16:30:01 -0300

    :heavy_plus_sign: Add Bcrypt and Guardian to dependencies

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Tecsolfacil.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:bcrypt_elixir, "~> 3.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
