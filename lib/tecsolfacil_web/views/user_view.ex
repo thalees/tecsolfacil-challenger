8605cf33400d9ca77394d245d8485f111f5eb77d
Thales Pereira <thalespereiradejesus@gmail.com>
2022-04-13 16:35:07 -0300

    :sparkles: Generate user model on application
    * This files was genereted using the command 'mix phx.gen.json Accounts User users email:unique password_hash:string'
    
    * Change the changeset method from user to perform email and password validations

---
+++ lib/tecsolfacil_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule TecsolfacilWeb.UserView do
+  use TecsolfacilWeb, :view
+  alias TecsolfacilWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      email: user.email,
+      password_hash: user.password_hash
+    }
+  end
+end
