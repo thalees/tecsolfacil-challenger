dd4fca50c36ffe848eee79fd2d95b4e9590e4ebd
Thales Pereira <thalespereiradejesus@gmail.com>
2022-04-13 18:58:59 -0300

    :green_heart: Remove unused method from Guardian module

--- lib/guardian.ex
+++ lib/guardian.ex
@@ -7,17 +7,9 @@ defmodule Tecsolfacil.Guardian do
     {:ok, sub}
   end
 
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
-  end
-
   def resource_from_claims(claims) do
     id = claims["sub"]
     resource = Tecsolfacil.Accounts.get_user!(id)
     {:ok, resource}
-  end
-
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
   end
 end
